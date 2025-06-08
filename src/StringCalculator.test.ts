4560a2b1680faa07d8b041ad3d9e74735141b28b
Diwanshu Chouragade <diwanshuc5@gmail.com>
2025-06-08 17:28:59 +0530

    feat: return number for single input

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -4,3 +4,7 @@ import { add } from './StringCalculator';
 test('returns 0 for empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('returns number for single input', () => {
+  expect(add("1")).toBe(1);
+});
--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,4 +1,5 @@
 export function add(numbers: string): number {
-  if (numbers === "") return 0;
-  return 0;
+  if (numbers === "") 
+    return 0;
+  return parseInt(numbers);
 }
